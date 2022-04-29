a2631b8ddaf71e2b7ce520b05df23c9a05bbfe78
patrickhalasik <patrickhalasik@gmail.com>
2022-04-29 20:22:24 +0200

    Correction bug page show

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -7,7 +7,7 @@
         <h1>{{ $show->title }} </h1>
 
         @if($show->poster_url)
-        <p><img src="{{ asset('images/'..$show->poster_url }}" alt="{{ $show->title }}" width="200"></p>
+        <p><img src="{{ asset('images/'.$show->poster_url) }}" alt="{{ $show->title }}" width="200"></p>
         @else
         <canvas width="200" height="100" style="border:1px solid #000000,"></canvas>
         @endif
@@ -16,14 +16,13 @@
         <p><strong>Lieu de création :</strong> {{ $show->location->designation }} </p>
         @endif
 
-        <p><strong>Prix :</strong> {{ $show->price }} €</p>
+        <p><strong>Prix :</strong> {{ $show->price }} €</p>
 
         @if($show->bookable)
         <p><em>Non réservable</em></p>
         @endif
     </article>
 
-    <nav><a href=" {{ route('show_index') }}">Retour à l'index</a></nav>
+    <nav><a href=" {{ route('show_index') }}">Retour à l'index</a></nav>
 
     @endsection
-
