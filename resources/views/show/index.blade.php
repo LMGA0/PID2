2a58ed08a95919f1149472d02abc2b22a9b17bf2
lmga0 <ludomarigliano@gmail.com>
2022-05-09 12:17:42 +0200

    Templates Show: affichage des représentations / correction a apporter

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -7,13 +7,20 @@
 
     <ul>
         @foreach($shows as $show)
-            <li>
-                <a href="{{ route('show_show', $show->id) }}">{{ $show->title }}</a>
-          @if($show->bookable)
-          <span>{{$show->price}} €</span>      
-          @endif
-            </li>
+        <li>
+            <a href="{{ route('show_show', $show->id) }}">{{ $show->title }}</a>
+            @if($show->bookable)
+            <span>{{$show->price}} €</span>      
+            @endif
+
+            @if($show->representations->count()==1)
+            - <span>1 représentation</span>
+            @elseif($show->representations->count()>1)
+            -<span>{{$show->representations->count()}} représentations</span>
+            @else
+            -<em>aucune représentation</em>
+            @endif
+        </li>
         @endforeach
     </ul>
-    
 @endsection
